620b9f4a787f6033d3b89e36f021a9b110230d27
Lucas Morais <morais.lucas.h@gmail.com>
2018-04-21 18:06:46 +0000

    Add script for fixing all .gitmodules

---
+++ fix_gitmodules.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sed -i.bak 's|git:\/\/github.com|https:\/\/github.com|' $(find -name [.]gitmodules)
+sed -i.bak 's|git:\/\/git.qemu.org|http:\/\/git.qemu.org\/git|' $(find -name [.]gitmodules)
+sed -i.bak 's|git:\/\/git.qemu-project.org\/|http:\/\/git.qemu.org\/git\/|' $(find -name [.]gitmodules)
+sed -i.bak 's|git:\/\/anongit.freedesktop.org\/pixman|http:\/\/anongit.freedesktop.org\/git\/pixman.git\/|' $(find -name [.]gitmodules)
+sed -i.bak 's|git:\/\/github.com\/rth7680\/qemu-palcode.git|https:\/\/github.com\/rth7680\/qemu-palcode.git|' $(find -name [.]gitmodules)
+
+git submodule sync --recursive
